1b207980ee1c2b46c08cb461c501cb0eb9622b6c
NTToan-Khiem <ntkhiem2510@gmail.com>
2023-04-05 09:03:31 +0700

    update v0.5 [Fix LCD interface]

--- SourceCode/text.c
+++ SourceCode/text.c
@@ -25,9 +25,14 @@
 //end define time remain state of system
 //Globle Variable 
 unsigned char g_operationCode = 0;
+unsigned char g_stateCodeLcd = 0;
+bool changeStateLcd = 1;
+unsigned char g_remainTimeLcd = 0;
+unsigned char g_remainTimeLcdMili = 0;
 unsigned char g_vPort = 0;
 unsigned char g_timeMili = 0;
 unsigned char g_timeSec = 0;
+unsigned char g_timeSecOld = 0;
 unsigned char g_codeSet[4] = {0,0,0,0};
 unsigned char g_codeEnter[4] = {1,1,1,1};
 unsigned char g_codeTemp[4] = {0,0,0,0};
@@ -52,20 +57,27 @@ void soundPushButton();
 //End list function hardwave interface
 //List function sorfware
 void scanKey();
+unsigned char scanKeyInside();
 bool checkCode();
+void printLcdU();
+void peintLcdL();
 void operationSystem();
+void operationLcd();
+void overString(char input1[17], char input2[17]);
 //End list function sorfware
+//Data of LCD display
+char stringLcdU[17];
+char stringLcdL[17];
+//end define
 //Interupt function lock or unlock door
 ISR(INT0_vect){ 	//Unlock function / allow to access cockpit
 	g_operationCode = 3; 	//unlock mode
 	g_timeSec = TIME_REMAIN_STATE_3;
-	writeDataLCD('0');
 }
 
 ISR(INT1_vect){	//lock function / denied to access cockpit
 	g_operationCode = 2; 	//lock mode
 	g_timeSec = TIME_REMAIN_STATE_2;
-	writeDataLCD('1');
 }
 //End define function interupt
 //Interupt function timer of system
@@ -79,10 +91,22 @@ ISR(TIMER0_OVF_vect){	//timer 0 use for timer of system with 10ms for interrupt
 			g_timeMili --;
 		}
 	}
+	if(g_remainTimeLcdMili > 0 || g_remainTimeLcd > 0){
+		if(g_remainTimeLcdMili == 0 && g_remainTimeLcd > 0){
+			g_remainTimeLcd --;
+			g_remainTimeLcdMili = 99;
+		}else if(g_remainTimeLcdMili > 0){
+			g_remainTimeLcdMili --;
+		}
+		if(g_remainTimeLcd == 0 && g_remainTimeLcdMili == 0){
+			writeCommandLCD(0x08);
+		}
+	}
 }
 //end define function timer of system
 //define function initial hardware 
 void initialTimer0();
+void initialLcd();
 void initialExternalInteruprt();
 void initialPinConfig();	//config pin
 void initializationSystem();
@@ -99,12 +123,10 @@ void initializationSystem();
 
 int main(void){
 	initializationSystem();
-	writeCommandLCD(0x0c);
-	writeDataLCD('y');
 	while(1){
 		scanKey();
-		setLCDPort(g_poiterOutside);
 		operationSystem();
+		operationLcd();
 	}
 }
 void setBitPB(unsigned char bit, bool logic){
@@ -198,17 +220,89 @@ void initialExternalInteruprt(){
 	EIFR=(1<<INTF1) | (1<<INTF0);
 	PCICR=(0<<PCIE2) | (0<<PCIE1) | (0<<PCIE0);
 }
-void scanKey(){
+unsigned char scanKeyInside(){
 	g_vPort |= 0x0f;
 	setVPort(g_vPort & 0xfe);
 	_delay_us(1);
 	if((PINC&(1<<3))==0){
-
+		_delay_ms(1);
+		while((PINC&(1<<3))==0);
+		soundPushButton();
+		return 0x31;
 	}else if((PINC&(1<<4))==0){
-
+		_delay_ms(1);
+		while((PINC&(1<<4))==0);
+		soundPushButton();
+		return 0x32;
 	}else if((PINC&(1<<5))==0){
-
+		_delay_ms(1);
+		while((PINC&(1<<5))==0);
+		soundPushButton();
+		return 0x33;
 	}
+	g_vPort |= 0x0f;
+	setVPort(g_vPort & 0xfd);
+	_delay_us(1);
+	if((PINC&(1<<3))==0){
+		_delay_ms(1);
+		while((PINC&(1<<3))==0);
+		soundPushButton();
+		return 0x34;
+	}else if((PINC&(1<<4))==0){
+		_delay_ms(1);
+		while((PINC&(1<<4))==0);
+		soundPushButton();
+		return 0x35;
+	}else if((PINC&(1<<5))==0){
+		_delay_ms(1);
+		while((PINC&(1<<5))==0);
+		soundPushButton();
+		return 0x36;
+	}
+	g_vPort |= 0x0f;
+	setVPort(g_vPort & 0xfb);
+	_delay_us(1);
+	if((PINC&(1<<3))==0){
+		_delay_ms(1);
+		while((PINC&(1<<3))==0);
+		soundPushButton();
+		return 0x37;
+	}else if((PINC&(1<<4))==0){
+		_delay_ms(1);
+		while((PINC&(1<<4))==0);
+		soundPushButton();
+		return 0x38;
+	}else if((PINC&(1<<5))==0){
+		_delay_ms(1);
+		while((PINC&(1<<5))==0);
+		soundPushButton();
+		return 0x39;
+	}
+	g_vPort |= 0x0f;
+	setVPort(g_vPort & 0xf7);
+	_delay_us(1);
+	if((PINC&(1<<3))==0){
+		_delay_ms(1);
+		while((PINC&(1<<3))==0);
+		soundPushButton();
+		return 0x2a;
+	}else if((PINC&(1<<4))==0){
+		_delay_ms(1);
+		while((PINC&(1<<4))==0);
+		soundPushButton();
+		return 0x30;
+	}else if((PINC&(1<<5))==0){
+		_delay_ms(1);
+		while((PINC&(1<<5))==0);
+		soundPushButton();
+		return 0x23;
+	}
+	return 0xff;
+}
+void scanKey(){
+	g_vPort |= 0x0f;
+	setVPort(g_vPort & 0xfe);
+	_delay_us(1);
 	if(g_operationCode == 0){
 		if((PINC&(1<<0))==0){
 			_delay_ms(1);
@@ -233,16 +327,9 @@ void scanKey(){
 			g_poiterOutside = 0;
 		}
 	}
-	
+	g_vPort |= 0x0f;
 	setVPort(g_vPort & 0xfd);
 	_delay_us(1);
-	if((PINC&(1<<3))==0){
-
-	}else if((PINC&(1<<4))==0){
-
-	}else if((PINC&(1<<5))==0){
-
-	}
 	if(g_operationCode == 0){
 		if((PINC&(1<<0))==0){
 			_delay_ms(1);
@@ -267,16 +354,9 @@ void scanKey(){
 			g_poiterOutside = 0;
 		}
 	}
-	
+	g_vPort |= 0x0f;
 	setVPort(g_vPort & 0xfb);
 	_delay_us(1);
-	if((PINC&(1<<3))==0){
-
-	}else if((PINC&(1<<4))==0){
-
-	}else if((PINC&(1<<5))==0){
-
-	}
 	if(g_operationCode == 0){
 		if((PINC&(1<<0))==0){
 			_delay_ms(1);
@@ -301,15 +381,9 @@ void scanKey(){
 			g_poiterOutside = 0;
 		}
 	}
+	g_vPort |= 0x0f;
 	setVPort(g_vPort & 0xf7);
 	_delay_us(1);
-	if((PINC&(1<<3))==0){
-
-	}else if((PINC&(1<<4))==0){
-
-	}else if((PINC&(1<<5))==0){
-
-	}
 	if(g_operationCode == 0){
 		if((PINC&(1<<0))==0){
 			_delay_ms(1);
@@ -326,17 +400,19 @@ void scanKey(){
 			_delay_ms(1);
 			while((PINC&(1<<2))==0);
 			writeDataLCD('s');
-			if(g_poiterOutside == 4){
-				g_poiterOutside = 0;
-				writeDataLCD('4');
-				if(checkCode()){
-					g_operationCode = 4;
-					g_timeSec = TIME_REMAIN_STATE_4;
+			if(g_stateCodeLcd != 4){
+				if(g_poiterOutside == 4){
+					g_poiterOutside = 0;
+					writeDataLCD('4');
+					if(checkCode()){
+						g_operationCode = 4;
+						g_timeSec = TIME_REMAIN_STATE_4;
+					}
+				}else if(g_operationCode != 2){
+					g_poiterOutside = 0;
+					g_operationCode = 1;
+					g_timeSec = TIME_REMAIN_STATE_1;
 				}
-			}else{
-				g_poiterOutside = 0;
-				g_operationCode = 1;
-				g_timeSec = TIME_REMAIN_STATE_1;
 			}
 		}
 		if(g_poiterOutside == 5){
@@ -346,7 +422,7 @@ void scanKey(){
 }
 void soundPushButton(){
 	setBitPD(7,1);
-	_delay_ms(100);
+	_delay_ms(50);
 	setBitPD(7,0);
 }
 bool checkCode(){
@@ -361,6 +437,9 @@ bool checkCode(){
 void operationSystem(){
 	switch(g_operationCode){
 		case 0:		//normal state
+			if(g_stateCodeLcd == 0){
+				g_operationCode = 7;
+			}
 			buzzer(0);
 			ledOpen(0);
 			ledFail(0);
@@ -414,6 +493,9 @@ void operationSystem(){
 			break;
 		case 4:
 			if((g_timeSec==0)&&(g_timeMili==0)){
+				buzzer(1);
+				_delay_ms(1000);
+				buzzer(0);
 				g_operationCode = 5;
 				g_timeSec = TIME_REMAIN_STATE_5;
 			}else{
@@ -454,6 +536,113 @@ void operationSystem(){
 			ledLock(1);
 			ledUnlock(1);
 			break;
+		case 7:
+			buzzer(0);
+			ledOpen(1);
+			ledFail(0);
+			ledAllow(0);
+			ledDenied(0);
+			ledLock(0);
+			ledUnlock(1);
+			if(g_stateCodeLcd != 0){
+				g_operationCode = 0;
+			}
+			break;
+	}
+}
+void operationLcd(){
+	unsigned char key = scanKeyInside();
+	if(changeStateLcd){
+		key = 0xfe;
+		changeStateLcd = 0;
+	}
+	if(key != 0xff){
+		g_remainTimeLcd = 10;
+		writeCommandLCD(0x0c);
+		if(g_stateCodeLcd == 0){
+			overString(stringLcdU, " Initialization ");
+			overString(stringLcdL, "1.Init     2.Off");
+			printLcdU();
+			printLcdL();
+			if(key == 0x31){
+				g_stateCodeLcd = 1;
+				changeStateLcd = 1;
+				key = 0xff;
+				overString(stringLcdL, "Enter code:     ");
+			}else if(key == 0x32){
+				writeCommandLCD(0x08);
+			}
+		}else if(g_stateCodeLcd == 1){
+			writeCommandLCD(0x0f);
+			unsigned char i = 0;
+			overString(stringLcdU, "Active code:    ");
+			if(key >= 0x30 && key <= 0x39){
+				if(g_poiterInside != 4){
+					stringLcdL[11+g_poiterInside] = key;
+				}
+				g_codeTemp[g_poiterInside] = key - 0x30;
+				g_poiterInside ++;
+				if(g_poiterInside == 5){
+					g_poiterInside = 0;
+				}
+			}else if(key == 0x23){
+				if(g_poiterInside == 4){
+					overString(stringLcdL, "Enter code:     ");
+					for(i=0; i<4; i++){
+						g_codeSet[i] = g_codeTemp[i];
+					}
+				}
+			}
+			for(i=0; i<4; i++){
+				stringLcdU[i+12] = g_codeSet[i]+0x30;
+			}
+			printLcdU();
+			printLcdL();
+			if(key == 0x2a){
+				g_stateCodeLcd = 2;
+				changeStateLcd = 1;
+				key = 0xff;
+			}
+		}else if(g_stateCodeLcd == 2){
+			overString(stringLcdU, "      Menu      ");
+			overString(stringLcdL, "1.Edit  2.Strict");
+			printLcdU();
+			printLcdL();
+			if(key == 0x31){
+				g_stateCodeLcd = 1;
+				changeStateLcd = 1;
+				key = 0xff;
+				overString(stringLcdL, "Enter code:     ");
+			}else if(key == 0x32){
+				g_stateCodeLcd = 3;
+				changeStateLcd = 1;
+				key = 0xff;
+			}
+		}else if(g_stateCodeLcd == 3){
+			overString(stringLcdU, "  Strict mode   ");
+			overString(stringLcdL, "Confirm press # ");
+			printLcdU();
+			printLcdL();
+			if(key == 0x23){
+				g_stateCodeLcd = 4;
+				changeStateLcd = 1;
+				key = 0xff;
+			}else if(key == 0x2a){
+				g_stateCodeLcd = 2;
+				changeStateLcd = 1;
+				key = 0xff;
+			}
+		}else if(g_stateCodeLcd == 4){
+			overString(stringLcdU, "  STRICT  MODE  ");
+			overString(stringLcdL, "----------------");
+			printLcdU();
+			printLcdL();
+			if(key == 0x2a){
+				g_stateCodeLcd = 2;
+				changeStateLcd = 1;
+				key = 0xff;
+			}
+		}
 	}
 }
 void ledOpen(bool input){
@@ -511,9 +700,53 @@ void initialPinConfig(){
 	PORTC = 0xFF; //all 1 to pull up
 	DDRD |= 0xe0; //bit 7 -> 5 is output else is input 
 }
+void initialLcd(){
+	g_remainTimeLcd = 20;
+	writeCommandLCD(0x0c);
+	writeCommandLCD(0x38);
+	_delay_ms(2000);
+	overString(stringLcdU, " Initialization ");
+	overString(stringLcdL, "Init LCD success");
+	printLcdU();
+	printLcdL();
+	_delay_ms(1000);
+}
 void initializationSystem(){
 	SREG |= 0x80;  //Global interrupt enable
 	initialPinConfig();
+	initialLcd();
+	overString(stringLcdL, "PinConfigSuccess");
+	printLcdL();
+	_delay_ms(500);
 	initialExternalInteruprt();
+	overString(stringLcdL, "InterruptSuccess");
+	printLcdL();
+	_delay_ms(500);
 	initialTimer0();
+	overString(stringLcdL, "InitTimerSuccess");
+	printLcdL();
+	buzzer(1);
+	_delay_ms(50);
+	buzzer(0);
+	_delay_ms(200);
+}
+void printLcdU(){
+	unsigned char i = 0;
+	writeCommandLCD(0x80);
+	for(i = 0; i<16 ; i++){
+		writeDataLCD(stringLcdU[i]);
+	}
+}
+void printLcdL(){
+	unsigned char i = 0;
+	writeCommandLCD(0xc0);
+	for(i = 0; i<16 ; i++){
+		writeDataLCD(stringLcdL[i]);
+	}
+}
+void overString(char input1[17], char input2[17]){
+	unsigned char i = 0;
+	for(i=0; i<17; i++){
+		input1[i] = input2[i];
+	}
 }
